430fe2e244891bad7eb3b1bf0e2c046d5808a072
Lorenzo Miechielsen <stagelorenzom@ventigrate.dev>
2025-06-10 11:14:52 +0200

    documents added

--- portofolio-site/src/pages/home.tsx
+++ portofolio-site/src/pages/home.tsx
@@ -5,16 +5,15 @@ import {
   Text,
   Title,
   Group,
+  Anchor,
 } from "@mantine/core";
-import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
 
   return (
     <Container
       fluid
-      px={'lg'}
+      px={"lg"}
       style={{
         justifyContent: "center",
         alignItems: "center",
@@ -24,27 +23,44 @@ function Home() {
         <Title order={1}>Portfolio – Lorenzo Miechielsen</Title>
 
         <Text size="lg">
-          Welkom op mijn professionele portfolio, het sluitstuk van mijn opleiding
-          Toegepaste Informatica – Application Development. Deze site geeft een overzicht
-          van de vaardigheden en kennis die ik heb verworven, geïllustreerd met projecten,
-          stages en praktische toepassingen. 
+          Welkom op mijn professionele portfolio, het sluitstuk van mijn
+          opleiding Toegepaste Informatica – Application Development. Deze site
+          geeft een overzicht van de vaardigheden en kennis die ik heb
+          verworven, geïllustreerd met projecten, stages en praktische
+          toepassingen.
         </Text>
 
         <Text size="md">
-          Mijn doel is om u als IT-professional een helder beeld te geven van mijn capaciteiten,
-          groeitraject en ambities binnen de softwareontwikkeling.
+          Mijn doel is om u als IT-professional een helder beeld te geven van
+          mijn capaciteiten, groeitraject en ambities binnen de
+          softwareontwikkeling.
         </Text>
 
         <Group mt="md">
-          <Button onClick={() => document.getElementById("over-mij")} variant="filled" color="#e14631">
+          <Anchor
+            href="#over-mij"
+            underline="never"
+            variant="text"
+            c="#e14631"
+          >
             Over Mij
-          </Button>
-          <Button variant="filled" color="#e14631" onClick={() => navigate("#realisaties")}>
-            Projecten
-          </Button>
-          <Button variant="filled" color="#e14631" onClick={() => navigate("#stage")}>
+          </Anchor>
+          <Anchor
+            href="#stage"
+            underline="never"
+            variant="text"
+            c="#e14631"
+          >
             Stage
-          </Button>
+          </Anchor>
+          <Anchor
+            href="#projecten"
+            underline="never"
+            variant="text"
+            c="#e14631"
+          >
+            Projecten
+          </Anchor>
         </Group>
       </Stack>
     </Container>
